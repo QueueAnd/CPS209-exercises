cb30281e7255c61050a9aaf4b95ef208d252dc1a
Andrew <qiu.andrew@stickyricegames.com>
2025-11-14 21:35:39 -0500

    Complete exercise 14

--- CCPS209_LABS_ANDREW_QIU_INTERMEDIATE/HTree.java
+++ CCPS209_LABS_ANDREW_QIU_INTERMEDIATE/HTree.java
@@ -23,7 +23,7 @@ public class HTree extends JPanel {
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, width, height);
         g2d.setColor(Color.BLACK);
-        render((double) width/2, (double) height/2, 3, 350, g2d);
+        render((double) width/2, (double) height/2, 2, 350, g2d);
         setPreferredSize(new Dimension(width, height));
     }
 
@@ -38,26 +38,29 @@ public class HTree extends JPanel {
 
     private static void render(double x, double y, int i, double len, Graphics2D g2) {
         if (len < CUTOFF) {
-            drawNode(x, y, g2);
+            drawNode(x, y, rng.nextBoolean(), g2);
+            drawNode(x, y-R, rng.nextBoolean(), g2);
         } else {
             g2.fillOval((int)(x-MINIR/2), (int)(y-MINIR/2), MINIR, MINIR);
-            int nx= (int)(DIRS[i][0]*len)+(int)x, ny= (int)(DIRS[i][1]*len)+(int)y;
-            g2.drawLine((int)x, (int)y, nx, ny);
+            int next= (((i+1)%DIRS.length)+DIRS.length)%DIRS.length;
+            int prev= (((i-1)%DIRS.length)+DIRS.length)%DIRS.length;
+            int nx1= (int)(DIRS[prev][0]*len)+(int)x, ny1= (int)(DIRS[prev][1]*len)+(int)y;
+            int nx2= (int)(DIRS[next][0]*len)+(int)x, ny2= (int)(DIRS[next][1]*len)+(int)y;
+
+            g2.drawLine((int)x, (int)y, nx1, ny1);
+            g2.drawLine((int)x, (int)y, nx2, ny2);
 
             double newLen=len/Math.sqrt(2);
-            render(nx, ny, (((i+1)%DIRS.length)+DIRS.length)%DIRS.length, newLen, g2);
-            render(nx, ny, (((i-1)%DIRS.length)+DIRS.length)%DIRS.length, newLen, g2);
+            render(nx1, ny1, (((i+1)%DIRS.length)+DIRS.length)%DIRS.length, newLen, g2);
+            render(nx2, ny2, (((i-1)%DIRS.length)+DIRS.length)%DIRS.length, newLen, g2);
         }
     }
 
-    private static void drawNode(double x, double y, Graphics2D g2) {
-        if (rng.nextBoolean()) {
+    private static void drawNode(double x, double y, boolean fill, Graphics2D g2) {
+        if(fill){
             g2.fillOval((int) (x-R/2), (int) (y), (int) R, (int) R);
-            g2.fillOval((int) (x-R/2), (int) (y-R), (int) R, (int) R);
         } else {
-            g2.setColor(Color.BLACK);
-            g2.drawOval((int) (x-R/2), (int) (y-R), (int) R, (int) R);
-            g2.drawOval((int) (x-R/2), (int) (y-R), (int) R, (int) R);
+            g2.drawOval((int) (x-R/2), (int) (y), (int) R, (int) R);
         }
     }
 }
